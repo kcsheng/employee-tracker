5ea4386eb4b83967deabd97dd870f7e0ad5e05a5
kcsheng <kacysheng@mail.com>
2021-08-15 22:26:59 +1000

    Add addRole, deleteRole, updateEmployeeRole, addDepartment and deleteDepartment

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const banner = require("./src/banner");
 const inquirer = require("inquirer");
 const sql = require("./config/mysqlConnect");
 const cTable = require("console.table");
-const { listenerCount } = require("events");
 
 const ask = () => {
   inquirer
@@ -29,7 +28,7 @@ const ask = () => {
           { value: "k", name: "Add Department" },
           { value: "l", name: "Delete Department" },
           { value: "m", name: "View All Departments" },
-          { value: "n", name: "View Utilised Budget" },
+          { value: "n", name: "View Staff Budget" },
           { value: "o", name: "Exit Program" },
         ],
       },
@@ -54,27 +53,33 @@ const ask = () => {
         case "f":
           viewEmployeesByDepartment();
           break;
+        case "g":
+          addRole();
+          break;
+        case "h":
+          deleteRole();
+          break;
+        case "i":
+          updateEmployeeRole();
+          break;
         case "j":
           viewAllRoles();
+          break;
+        case "k":
+          addDepartment();
+          break;
+        case "l":
+          deleteDepartment();
           break;
         case "m":
           viewAllDepartments();
           break;
-
+        case "n":
+          viewStaffBudget();
+          break;
         case "o":
           process.exit();
       }
-
-      // case "c":
-      //   updateEmployeeRole();
-      // case "e":
-      //   addRole();
-      // case "f":
-      //   viewAllDepartments();
-      // case "g":
-      //   //   addDepartment();
-      // }
-      // answer.task === "h" ? process.exit() : ask();
     })
     .catch((err) => console.error(err));
 };
@@ -87,6 +92,229 @@ const init = () => {
 init();
 
 // Query functions
+
+function viewStaffBudget() {
+  sql
+    .promise()
+    .query()
+    .then()
+    .catch((err) => console.error(err));
+}
+
+function deleteDepartment() {
+  sql
+    .promise()
+    .query(`SELECT id, name FROM department`)
+    .then((data) => JSON.parse(JSON.stringify(data[0])))
+    .then((objectified) => {
+      const departmentIdsAndNames = objectified;
+      const departmentNames = departmentIdsAndNames.map(
+        (department) => department.name
+      );
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "department",
+            message: "Which department would you like to delete?",
+            choices: departmentNames,
+          },
+        ])
+        .then((result) => {
+          // Get role id and name
+          const [department] = departmentIdsAndNames.filter((department) => {
+            return result.department === department.name;
+          });
+          sql.query(`DELETE FROM department WHERE id = ${department.id}`);
+          console.log(`The department ${department.name} has been deleted!`);
+          ask();
+        })
+        .catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
+}
+
+function addDepartment() {
+  sql
+    .promise()
+    .query(`SELECT * FROM department`)
+    .then((data) => JSON.parse(JSON.stringify(data[0])))
+    .then((results) => {
+      const departmentIdsAndNames = results;
+      const departmentNames = departmentIdsAndNames.map(
+        (department) => department.name
+      );
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "department",
+            message: "What department would you like to add?",
+            validate: (input) => {
+              const regexp = new RegExp(/^[0-9 a-z,.'-@]+$/, "i");
+              return regexp.test(input) && input.length > 0
+                ? true
+                : "Input not valid!";
+            },
+          },
+        ])
+        .then((data) => {
+          sql.query(`INSERT INTO department (name) VALUES (?)`, [
+            `${data.department}`,
+          ]);
+          console.log(`New department ${data.department} has been added!`);
+          ask();
+        })
+        .catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
+}
+
+function updateEmployeeRole() {
+  sql
+    .promise()
+    .query(
+      `SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee`
+    )
+    .then(([rows, fields]) => JSON.parse(JSON.stringify(rows)))
+    .then((objectified) => {
+      const employeesIdsAndNames = objectified;
+      const employeeNames = employeesIdsAndNames.map(
+        (employee) => employee.name
+      );
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "name",
+            message: "Which employee would you like to update role for?",
+            choices: employeeNames,
+          },
+        ])
+        .then((person) => {
+          const [employee] = employeesIdsAndNames.filter((employee) => {
+            return employee.name === person.name;
+          });
+
+          sql.query(`SELECT id, title FROM role`, (err, data) => {
+            if (err) throw err;
+            const roles = JSON.parse(JSON.stringify(data));
+            const titles = roles.map((role) => role.title);
+            inquirer
+              .prompt([
+                {
+                  type: "list",
+                  name: "title",
+                  message: `Which role would you like to assign to ${employee.name}?`,
+                  choices: titles,
+                },
+              ])
+              .then((result) => {
+                const [roleIdAndTitle] = roles.filter((role) => {
+                  return role.title === result.title;
+                });
+                sql.query(
+                  `UPDATE employee SET role_id = ${roleIdAndTitle.id} WHERE id = ${employee.id}`
+                );
+                console.log(`The employee ${employee.name}'s role is updated!`);
+                ask();
+              })
+              .catch((err) => console.error(err));
+          });
+        })
+        .catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
+}
+
+function deleteRole() {
+  sql
+    .promise()
+    .query(`SELECT id, title FROM role`)
+    .then((data) => JSON.parse(JSON.stringify(data[0])))
+    .then((objectified) => {
+      const roleIdsAndTitles = objectified;
+      const titles = roleIdsAndTitles.map((role) => role.title);
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "title",
+            message: "Which role would you like to delete?",
+            choices: titles,
+          },
+        ])
+        .then((result) => {
+          // Get role id and name
+          const [role] = roleIdsAndTitles.filter((role) => {
+            return result.title === role.title;
+          });
+          sql.query(`DELETE FROM role WHERE id = ${role.id}`);
+          console.log(`The role ${role.title} has been deleted!`);
+          ask();
+        })
+        .catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
+}
+
+function addRole() {
+  sql
+    .promise()
+    .query(`SELECT * FROM department`)
+    .then((data) => JSON.parse(JSON.stringify(data[0])))
+    .then((objectified) => {
+      const departmentIDsAndNames = objectified;
+      const departments = departmentIDsAndNames.map(
+        (department) => department.name
+      );
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "title",
+            message: "What role would you like to add?",
+            validate: (input) => {
+              const regexp = new RegExp(/^[0-9 a-z,.'-@]+$/, "i");
+              return regexp.test(input) && input.length > 0
+                ? true
+                : "Input not valid!";
+            },
+          },
+          {
+            type: "input",
+            name: "salary",
+            message: "How much is the salary?",
+            validate: (input) => {
+              const regexp = new RegExp(/^[0-9]+$/);
+              return regexp.test(input) && parseInt(input) > 1000
+                ? true
+                : "Input not valid!";
+            },
+          },
+          {
+            type: "list",
+            name: "department",
+            message: "Which department does this role belong?",
+            choices: departments,
+          },
+        ])
+        .then((newRole) => {
+          const [department] = departmentIDsAndNames.filter((department) => {
+            return newRole.department === department.name;
+          });
+          sql.query(
+            `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ${department.id})`,
+            [`${newRole.title}`, `${newRole.salary}`]
+          );
+          console.log(`New role ${newRole.title} has been created!`);
+          ask();
+        })
+        .catch((err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
+}
+
 function viewEmployeesByDepartment() {
   sql
     .promise()
@@ -106,7 +334,7 @@ function viewEmployeesByDepartment() {
         .then((selection) => {
           const departmentName = selection.department;
           sql.query(
-            `SELECT employee.id, CONCAT(first_name, " ", last_name) AS employee, title, salary FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = "${departmentName}"`,
+            `SELECT e.id, CONCAT(e.first_name, " ", e.last_name) AS employee, title, salary, CONCAT(m.first_name, " ", m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = "${departmentName}"`,
             (err, result) => {
               if (err) throw err;
               console.table(result);
@@ -342,7 +570,8 @@ function addEmployee() {
             (err, id) => {
               if (err) throw err;
               sql.query(
-                `INSERT INTO employee (first_name, last_name, role_id) VALUES ("${answers.first_name}", "${answers.last_name}", ${id[0].id})`
+                `INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)`,
+                [`${answers.first_name}`, `${answers.last_name}`, `${id[0].id}`] // Prepared statement
               );
             }
           );
@@ -393,38 +622,3 @@ function addEmployee() {
     })
     .catch((err) => console.error(err));
 }
-
-// function addDepartment(name) {
-//   sql
-//     .promise()
-//     .query(`INSERT INTO department (name) VALUES (?)`, [name])
-//     .then(() => ask())
-//     .catch((err) => console.error(err));
-// }
-
-// function addRole(name, salary, department_id) {
-//   sql
-//     .promise()
-//     .query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [
-//       name,
-//       salary,
-//       department_id,
-//     ])
-//     .then(() => ask())
-//     .catch((err) => console.error(err));
-// }
-
-// function updateEmployeeRole(employee_id, role_id) {
-//   sql
-//     .promise()
-//     .query(`UPDATE employee SET role_id = ? WHERE id = ?`, [
-//       role_id,
-//       employee_id,
-//     ])
-//     .then(() => ask())
-//     .catch((err) => console.error(err));
-// }
-
-// const roleNames = [];
-// data[0].forEach((row) => roleNames.push(row.title));
-// return roleNames;
